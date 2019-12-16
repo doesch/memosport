d2cc4f0f5e1313fefbfa565bb6df0ed104cce48c
doesch <skewer@gmx.de>
2019-12-16 17:04:55 +0100

    updated gitignore

--- Mysql Backup/Export.bat
+++
@@ -1,9 +0,0 @@
-set CurrentPath=%~dp0
-
-::navigate to mysql folder where mysqldump exists
-cd "C:\xampp\mysql\bin"
-
-::create dump
-mysqldump -u root -h localhost memosport > "%CurrentPath%memosport.sql"
-
-pause
--- Mysql Backup/Import.bat
+++
@@ -1,9 +0,0 @@
-set CurrentPath=%~dp0
-
-::navigate to mysql folder where mysqldump exists
-cd "C:\xampp\mysql\bin"
-
-::import
-mysql -u root memosport < "%CurrentPath%memosport.sql"
-
-pause
