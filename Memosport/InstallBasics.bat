1d28fd8d32b78d2d8da241ffb5552b6975fe6882
doesch <skewer@gmx.de>
2019-12-17 10:39:38 +0100

    removed package-lock.json because this file is not required.

--- Memosport/InstallBasics.bat
+++ Memosport/InstallBasics.bat
@@ -1,12 +1,14 @@
+::install from package.json
+call npm install
 ::create Gulpfile.js
-call npm init
+::call npm init
 ::install libraries for taskrunners
-call npm install gulp --save-dev gulp-less --save-dev gulp-clean-css --save-dev gulp-concat --save-dev
+::call npm install gulp --save-dev gulp-less --save-dev gulp-clean-css --save-dev gulp-concat --save-dev
 ::link to local gulp in project
 call npm link gulp
 ::install standard js-libraries
-call npm install --save-dev jquery --save-dev requirejs
+::call npm install --save-dev jquery --save-dev requirejs
 ::install jquery visibility in Typescript
-call npm install --save-dev @types/jquery
+::call npm install --save-dev @types/jquery
 :: install knockout visibility in Typescript:
 ::call npm install --save-dev @types/knockout
