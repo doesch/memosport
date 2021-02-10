9adf85cf19c69452cb05fca50801dbc1ea014084
Simon Dötsch <doetsch@jam-software.de>
2021-02-10 10:39:06 +0100

    Added OpenApiController which is public accessible and currently delivers the master-system. Added new publish profile for my mobile-office notebook.

---
+++ Memosport/Controllers/OpenApiController.cs
@@ -0,0 +1,42 @@
+﻿using Memosport.Data;
+using Memosport.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memosport.Controllers
+{
+    /// <summary> public access (no authentication required). </summary>
+    /// <remarks> Doetsch, 10.02.2021. </remarks>
+    [Route("[controller]")]
+    [ApiController]
+    public class OpenApiController : BaseApiController
+    {
+        /// <summary> The database context. </summary>
+        private MemosportContext _context;
+
+        public OpenApiController(MemosportContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary> (An Action that handles HTTP GET requests) master system. </summary>
+        /// <remarks> Doetsch, 10.02.2021. </remarks>
+        /// <returns> An asynchronous result that yields an IActionResult. </returns>
+        [HttpGet("MasterSystem")]
+        public async Task<IActionResult> MasterSystem()
+        {
+            var lBoxId = 20;
+
+            // get indexcardboxes of the master system
+            var lQuery = _context.IndexCards.Select(x => x).Where(x => x.IndexCardBoxId == lBoxId && x.Id <= 1220).OrderBy(x => x.Question);
+            var lResult = await lQuery.ToListAsync();
+
+            return Json(lResult);
+        }
+    }
+}
